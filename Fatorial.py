9ff666850288faf421f7c5c920358d9c9e322fca
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-01-06 18:54:26 -0300

    fim

---
+++ Fatorial.py
@@ -0,0 +1,9 @@
+num = int(input("Fatorial de qual numero ? "))
+
+f=1
+i=1
+
+while num >= f:
+    i = i * f
+    f += 1
+print(i)
--- Fibonacci.py
+++ Fibonacci.py
@@ -6,6 +6,18 @@ def Fibonacci(num):
         temp = a
         a = b
         b = temp + a
-
 for f in Fibonacci(10):
     print (f)
+
+#list
+def Fibonacci2(num):
+    a = 0
+    b = 1
+    result = []
+    for i in range(num):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + a
+    return result
+print(Fibonacci2(10))
