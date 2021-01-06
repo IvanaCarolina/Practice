22fadd8d72f01a58043b6d81a48d9fe2ed3ebdfd
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-01-06 10:28:25 -0300

    Create try.py
    dei inicio

---
+++ try.py
@@ -0,0 +1,13 @@
+#error exceptions
+
+while True:
+    try:
+        age = int(input('whats your age ? '))
+        10/age
+    except ValueError:
+        print('a number please')
+    except ZeroDivisionError:
+        print('a number higher than zero')
+    else:
+        print('Thanks')
+        break
