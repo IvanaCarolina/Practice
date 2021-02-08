acb3e73b1705ca3d1dfb1b196f1db9f995c761ba
Ivana Carolina <75748698+IvanaCarolina@users.noreply.github.com>
2021-02-08 18:26:29 -0300

    Update regular_expressions.py

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,8 +1,17 @@
+#email checker
 import re
 
 
 pattern = re.compile(r"^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$")
-string = 'carollolivei7'
+string = 'carollolivei7@gmail.com'
+password = re.compile(r"[A-Za-z0-9$%#@]{7,}[0-9]")
+string2 = 'Ivana$@1788'
 
 a = pattern.search(string)
+check = password.fullmatch(string2)
+
 print(a)
+print(check)
+
+#[A-Za-z0-9$%#@]{7,}[0-9]\d
+#[A-Za-z0-9$%#@]{7,}[0-9]
